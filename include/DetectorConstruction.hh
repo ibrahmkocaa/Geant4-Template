79df32c737d9a555b54af2819fe9fd5678568d2a
ibo <ibo@ibos-MacBook-Air.local>
2025-06-20 20:32:56 +0300

    template

---
+++ include/DetectorConstruction.hh
@@ -0,0 +1,14 @@
+#ifndef DETECTOR_CONSTRUCTION_HH
+#define DETECTOR_CONSTRUCTION_HH
+
+#include "G4VUserDetectorConstruction.hh"
+
+class DetectorConstruction : public G4VUserDetectorConstruction {
+public:
+    DetectorConstruction() = default;
+    ~DetectorConstruction() = default;
+
+    virtual G4VPhysicalVolume* Construct() override;
+};
+
+#endif
---
+++ src/DetectorConstruction.cc
@@ -0,0 +1,19 @@
+#include "DetectorConstruction.hh"
+#include "G4Box.hh"
+#include "G4LogicalVolume.hh"
+#include "G4PVPlacement.hh"
+#include "G4NistManager.hh"
+#include "G4SystemOfUnits.hh"
+
+G4VPhysicalVolume* DetectorConstruction::Construct() {
+    G4NistManager* nist = G4NistManager::Instance();
+
+    G4Material* worldMat = nist->FindOrBuildMaterial("G4_AIR");
+
+    G4Box* solidWorld = new G4Box("World", 1.0*m, 1.0*m, 1.0*m);
+    G4LogicalVolume* logicWorld = new G4LogicalVolume(solidWorld, worldMat, "World");
+    G4VPhysicalVolume* physWorld = new G4PVPlacement(
+        0, G4ThreeVector(), logicWorld, "World", nullptr, false, 0, true);
+
+    return physWorld;
+}
